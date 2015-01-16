4f608e05867579b0f35056039b7802e0a43aec41
MrUse <mruse@users.noreply.github.com>
2015-01-17 02:30:42 +0800

    Update deny.sh

--- deny.sh
+++ deny.sh
@@ -10,7 +10,9 @@ export PATH=$PATH
 TCP_DENY="/etc/hosts.deny"
 WEB_DENY="/etc/nginx/vhosts/acces-deny.inc"
 
-[ ! -f "./hosts.deny" ] && echo -e "" && exit;
+[ ! -f "./hosts.deny" ] && echo -e "\e[1;31mBlacklist file is not exists! \e[0m" && exit 1;
+[ ! -f "$WEB_DENY" ] && echo -e "\e[1;31mWeb Server config file \"$WEB_DENY\" is not exists! \e[0m" && exit 1;
+
 for ip in $(cat hosts.deny|awk '{print $1}')
 do
     echo "ALL:$ip"   >> $TCP_DENY
